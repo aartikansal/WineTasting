30414a35d616d30d5991de154616a9208e99a62b
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-07 22:17:38 -0500

    Add files via upload

---
+++ SolveWine.java
@@ -0,0 +1,21 @@
+public class SolveWine {
+    public static void main(String[] args){
+
+        getDeliciousDrink().taste();
+        System.out.println(getWine().getCelebrationName());
+        System.out.println(getBubblyWine().getCelebrationName());
+        System.out.println(getWine().getCelebrationName());
+    }
+
+    public static Drink getDeliciousDrink() {
+        return new Wine();
+    }
+
+    public static Wine getWine() {
+        return new Wine();
+    }
+
+    public static Wine getBubblyWine() {
+        return new BubblyWine();
+    }
+}
